ea1d6c2811d8090c89438e37facb6e22642c03bb
Ryan Durand <ryandurand70@gmail.com>
2021-08-25 20:51:19 -0400

    Added groundwork for community playlist functionality: /list route with mongoose/mongoDB backend database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // Module declarations
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Set app to use EJS
 const app = express();
@@ -10,6 +11,50 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(express.static("public"));
+
+// Connect to Mongoose database backend
+mongoose.connect("mongodb://localhost:27017/musicDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Schema for the list database
+const albumsSchema = mongoose.Schema({
+    artist: {
+      type: String,
+      required: [true, "All entries need an artist name."]
+    },
+    title: {
+      type: String,
+      required: [true, "All entries need a project title."]
+    },
+    listen: {
+        type: String
+    }
+});
+
+const Album = mongoose.model("Album", albumsSchema);
+
+// example albums that will populate the backlog if it is empty
+const beUpAHello = new Album({
+    artist: "Squarepusher",
+    title: "Be Up A Hello",
+    listen: "https://www.youtube.com/watch?v=ODtzaxx-ifc&list=OLAK5uy_lb3RkVZOSxQTx1XBHQ7PeAIzQ_Jd8AAjk&index=1"
+});
+  
+const melee = new Album({
+    artist: "Dogleg",
+    title: "Melee",
+    listen: "https://www.youtube.com/watch?v=_BrZUwzrvVI&list=PLNcHHgfUCHDaCKUReDsh6W8KDlQcdPmtT"
+});
+  
+const noDream = new Album({
+    artist: "Jeff Rosenstock",
+    title: "NO DREAM",
+    listen: "https://www.youtube.com/watch?v=T2UVigK4dA8&list=PL8M_6pUgzQuUySDUembnytFIYKskRzj_D"
+});
+
+const defaultItems = [beUpAHello, melee, noDream];
 
 // Routing to homepage
 app.get("/", function(req, res){
@@ -24,6 +69,29 @@ app.get("/vault", function(req,res){
 // Routing to most wanted page
 app.get("/mostwanted", function(req,res){
     res.render("mostwanted");
+});
+
+// Routing to community playlist page
+app.get("/list", function(req, res) {
+    Album.find(function(err, albums) {
+        //insert default albums if the list is empty
+        if (albums.length === 0) {
+            Album.insertMany(defaultItems, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved albums.")
+                }
+            });
+            //kickback to root route then skip straight to the last else block
+            res.redirect("/list");
+        } else {
+            res.render("list", {
+                listTitle: "Community Playlist",
+                newListItems: albums
+            });
+        }
+    });
 });
 
 // Set port to Heroku's port or localhost:3000
